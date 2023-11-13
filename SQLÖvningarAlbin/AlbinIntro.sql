ffd1040668ef05aee09affe521848f101df64645
albin-hedberg <143542598+albin-hedberg@users.noreply.github.com>
2023-11-13 13:52:55 +0100

    Update AlbinIntro.sql
    Senaste

--- SQLÖvningarAlbin/AlbinIntro.sql
+++ SQLÖvningarAlbin/AlbinIntro.sql
@@ -1,53 +1,52 @@
 --CREATE DATABASE DbDemo;
 
 --1
---CREATE DATABASE DjurDB;
+CREATE DATABASE DjurDB;
 
---USE DjurDB;
+USE DjurDB;
 
 --2
---CREATE TABLE DjurTbl(
---	Id int PRIMARY KEY,
---	Namn nvarchar(max) not null
---);
+CREATE TABLE DjurTbl(
+	Id int PRIMARY KEY,
+	Namn nvarchar(max) not null
+);
 
 --3
---INSERT INTO DjurTbl(Id, Namn)
---VALUES (1, 'Hamster'), (2, 'Hund'), (3, 'Katt'), (4, 'Kanin');
+INSERT INTO DjurTbl(Id, Namn)
+VALUES (1, 'Hamster'), (2, 'Hund'), (3, 'Katt'), (4, 'Kanin');
 
 --4
---SELECT * FROM DjurTbl;
+SELECT * FROM DjurTbl;
 
 --5
---SELECT * FROM DjurTbl WHERE Namn LIKE '%a%';
+SELECT * FROM DjurTbl WHERE Namn LIKE '%a%';
 
 --6
---DROP TABLE DjurTbl;
---USE DbDemo;
---USE master;
---DROP DATABASE DjurDB;
+DROP TABLE DjurTbl;
+USE master;
+DROP DATABASE DjurDB;
 
 --7
---CREATE DATABASE KlassenDb;
+CREATE DATABASE KlassenDb;
 
 USE KlassenDb;
 
 --8
---CREATE TABLE Students(
---	[Id] int PRIMARY KEY,
---	[Förnamn] nvarchar(max) not null,
---	[Efternamn] nvarchar(max) not null,
---	[Ålder] int,
---	[Hemort] nvarchar(max) not null
---);
+CREATE TABLE Students(
+	[Id] int PRIMARY KEY,
+	[Förnamn] nvarchar(max) not null,
+	[Efternamn] nvarchar(max) not null,
+	[Ålder] int,
+	[Hemort] nvarchar(max) not null
+);
 
 --9
---INSERT INTO Students
---VALUES
---	(1, 'Kevin', 'Sahlén', 33, 'Göteborg'),
---	(2, 'Maria', 'Edstrom', 28, 'Stockholm'),
---	(3, 'Sebastian', 'Moya Schöndorff', 37, 'Göteborg'),
---	(4, 'Lotta', 'Harmonen', 28, 'Mikkeli'),
---	(5, 'Albin', 'Hedberg Abarahamsson', 30, 'Mölndal');
+INSERT INTO Students
+VALUES
+	(1, 'Kevin', 'Sahlén', 33, 'Göteborg'),
+	(2, 'Maria', 'Edstrom', 28, 'Stockholm'),
+	(3, 'Sebastian', 'Moya Schöndorff', 37, 'Göteborg'),
+	(4, 'Lotta', 'Harmonen', 28, 'Mikkeli'),
+	(5, 'Albin', 'Hedberg Abarahamsson', 30, 'Mölndal');
 
 SELECT * FROM Students;
