535f8a494cf8e456c60917990c358f4c6a337d0c
albin-hedberg <143542598+albin-hedberg@users.noreply.github.com>
2023-11-13 13:22:09 +0100

    Create AlbinIntro.sql
    Introduktionsövningar 1-9

---
+++ SQLÖvningarAlbin/AlbinIntro.sql
@@ -0,0 +1,53 @@
+--CREATE DATABASE DbDemo;
+
+--1
+--CREATE DATABASE DjurDB;
+
+--USE DjurDB;
+
+--2
+--CREATE TABLE DjurTbl(
+--	Id int PRIMARY KEY,
+--	Namn nvarchar(max) not null
+--);
+
+--3
+--INSERT INTO DjurTbl(Id, Namn)
+--VALUES (1, 'Hamster'), (2, 'Hund'), (3, 'Katt'), (4, 'Kanin');
+
+--4
+--SELECT * FROM DjurTbl;
+
+--5
+--SELECT * FROM DjurTbl WHERE Namn LIKE '%a%';
+
+--6
+--DROP TABLE DjurTbl;
+--USE DbDemo;
+--DROP DATABASE DjurDB;
+
+--7
+--CREATE DATABASE KlassenDb;
+
+USE KlassenDb;
+
+--8
+--CREATE TABLE Students(
+--	[Id] int PRIMARY KEY,
+--	[Förnamn] nvarchar(max) not null,
+--	[Efternamn] nvarchar(max) not null,
+--	[Ålder] int,
+--	[Hemort] nvarchar(max) not null
+--);
+
+--9
+--INSERT INTO Students
+--VALUES
+--	(1, 'Albin', 'Hedberg Abarahamsson', 30, 'Göteborg'),
+--	(2, 'Kevin', 'Sahlén', 0, 'Göteborg'),
+--	(3, 'Maria', 'Edstrom', 0, 'H'),
+--	(4, 'Sebastian', 'Moya Schöndorff', 0, 'F'),
+--	(5, 'Lotta', 'Harmonen', 0, '');
+
+--SELECT * FROM Students;
+
